713dd74f9637dbd07907a51af968d17258e1741b
Guilherme Oliveira <dipponippo@gmail.com>
2025-07-14 20:33:52 -0300

    Add log creator.

--- file_reader.py
+++ file_reader.py
@@ -7,3 +7,9 @@ with open('pokedex.txt', 'r') as file:
     
 print(list_from_text)
 
+with open('log.txt', 'w') as log_file:
+    log_file.write(f"-- LOG START --\nMy current program opens the pokedex.txt file.\nThat file\
+contains the text of 6 pokémon.\nIt then adds each pokémon name to an empty list.\nAnd prints that list.\n-- LOG END --\n")
+    
+
+
